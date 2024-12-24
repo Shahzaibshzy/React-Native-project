5f9779989d79a0a16900fe3eba7d5bd8a3710019
Shahzaibshzy <shhzbsaleem@gmail.com>
2024-12-24 21:54:57 +0500

    small change

--- client/App.js
+++ client/App.js
@@ -1,13 +1,9 @@
 import React from "react";
 import FriendListScreen from "./components/FriendListScreen";
-import styles from "./components/Style";
-import { View } from "react-native-web";
 
 
 const App = () => {
   return (
-    
-   
     <FriendListScreen />
   );
 };
